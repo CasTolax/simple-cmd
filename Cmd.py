74ff7fc0be884490af1385c925dfcc3751fc1f94
CasTolax <cantuncer32@gmail.com>
2025-05-10 05:37:36 +0300

    1.0.1 Cmd.py
    Only new features were added and code readability was improved.

--- Cmd.py
+++ Cmd.py
@@ -1,10 +1,14 @@
 import time
 import os
-import getpass  
+import getpass
+import random
+from datetime import datetime
+import json
 
+PASSWORD = "Can123"
+LOG_DOSYA = "log.txt"
 
 class Arayuz:
-
     def baslatma_animasyonu(self):
         print("program başlatılıyor...")
         time.sleep(2.4)
@@ -41,78 +45,205 @@ class Arayuz:
 
 
 class Terminal:
+    def __init__(self):
+        self.baslangıc = datetime.now()
+        self.komut_gecmisi = []
+        self.ayarlar = {}
+        self.ayarları_yukle()
 
     def terminal_govdesi(self):
         print("help yazarak kullanabileceğiniz komutlara bakabilirsiniz.\n")
         while True:
             try:
                 gövde = input("CeaserCmd>> ").strip().lower()
+                self.komut_gecmisi.append(gövde)
+
+                timestamp = time.strftime("[%Y-%m-%d %H:%M:%S]")
+                with open(LOG_DOSYA, "a", encoding="utf-8") as f:
+                    f.write(f"{timestamp} {gövde}\n")
 
                 if gövde == "help":
-                    print("""
+                    self.komut_help()
+                elif gövde == "cd":
+                    self.komut_cd()
+                elif gövde == "ls":
+                    self.komut_ls()
+                elif gövde == "calc":
+                    self.komut_calc()
+                elif gövde == "datetime":
+                    self.uptime()
+                elif gövde == "info":
+                    self.komut_info()
+                elif gövde == "timelook":
+                    self.komut_timelook()
+                elif gövde == "log":
+                    self.komut_log()
+                elif gövde == "log --clear":
+                    self.komut_log_clear()
+                elif gövde == "colorama":
+                    print("sorry this isn't exists")
+                elif gövde == "userinfo":
+                    self.komut_userinfo()
+                elif gövde == "clear":
+                    os.system("cls" if os.name == "nt" else "clear")
+                elif gövde == "history":
+                    self.komut_history()
+                elif gövde == "exit":
+                    print("programdan çıkıldı")
+                    break
+                elif gövde == "yzgame":
+                    self.komut_yzgame()
+                elif gövde == "lucky":
+                    self.komut_lucky()
+                else:
+                    print("Geçersiz komut. 'help' yazarak komutları görebilirsiniz.")
+
+            except Exception as e:
+                print(f"Hata oluştu: {e}")
+
+    def komut_help(self):
+        print("""
 Kullanabileceğiniz komutlar:
   cd         - Klasör değiştir
+  clear      - sil                        
   ls         - Dosyaları listele
+  calc       - basit hesap makinesi
+  datetime   - işlem zamanı veya bilgisiyar açık kalma süresi
   info       - Sistem bilgisi
   timeLook   - Şu anki zamanı göster
-  log        - Kayıt sistemi (yok)
+  log        - Kayıt sistemi | --clear (etkin)
   colorama   - Renkli yazı örneği (yok)
   userinfo   - Kullanıcı adı göster
+  history    - komut geçmişi
   exit       - Terminalden çık
+  yzgame     - yazı/tura şans oyunu
+  lucky      - günün şanslı sayısını gösterir
 """)
-                elif gövde == "cd":
-                    secim = input("gitmek istediğiniz dosya: ").strip()
-                    if secim == "downloads_file":
-                        print("geçildi")
-                    elif secim == "files":
-                        print("files dosyasına geçildi")
-                    elif secim == "documents_file":
-                        print("documents dosyasına geçildi")
-                    elif secim == "saves_file":
-                        print("saves_file dosyasına geçildi")
-                    else:
-                        print("hata: yanlış veya olmayan dosya")
+        
 
-                elif gövde == "ls":
-                    print("""
+
+    def ayarları_yukle(self):
+        try:
+            with open("ayarlar.json","r", encoding="utf-8") as f:
+                self.ayarlar = json.loads(f)
+                print("ayarlar kaydedildi")
+        except FileNotFoundError:
+            print("ayar dosyası bulunamadı")
+            self.ayarlar = {"tema":"koyu","log":True}
+        except json.JSONDecodeError:
+            print("ayar dosyası bozuk.varsayılanlar yüklendi")
+            self.ayarlar = {"tema":"koyu", "log":True}
+
+
+    def ayarları_kaydet(self):
+        with open("ayarlar.json","w", encoding="utf-8") as f:
+            json.dump(self.ayarlar,f, indent=4, ensure_ascii=False)
+            print("ayarlar kaydedildi")           
+
+    def komut_cd(self):
+        secim = input("gitmek istediğiniz dosya: ").strip()
+        klasorler = ["downloads_file", "files", "documents_file", "saves_file"]
+        if secim in klasorler:
+            print(f"{secim} dosyasına geçildi")
+        else:
+            print("hata: yanlış veya olmayan dosya")
+
+    def komut_ls(self):
+        print("""
 --files--
     downloads_file
     files
     documents_file
     saves_file""")
 
-                elif gövde == "info":
-                    print("--sistem bilgileri--")
-                    print(f"OS Adı: {os.name}")
-                    try:
-                        print(os.uname())
-                    except AttributeError:
-                        print("uname() bu sistemde desteklenmiyor.")
-                    print(f"PATH: {os.getenv('PATH')}")
-                    print("kullanılan terminal türü: Ceaser Terminal")
+    def komut_calc(self):
+        hesap = input("hesaplamak istediğiniz işlemi giriniz (ör: 2+2): ")
+        if all(c in "0123456789+-*/(). " for c in hesap):
+            print(eval(hesap))
+        else:
+            print("geçersiz karakter")
 
-                elif gövde == "timelook":
-                    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                    print(f"Güncel zaman: {current_time}")
 
-                elif gövde == "log":
-                    print("sorry this isn't exists")
+    def uptime(self):
+        suan = datetime.now()
+        fark = suan - self.baslangıc
+        saat,dakika = divmod(fark.seconds,3600)
+        dakika,saniye = divmod(dakika,60)
+        print(f"program açık kalmak süresi: {fark.days} gün, {saat} saat, {dakika} dakika, {saniye} saniye")
 
-                elif gövde == "colorama":
-                    print("sorry this isn't exists")
 
-                elif gövde == "userinfo":
-                    username = getpass.getuser()
-                    print(f"Kullanıcı adı: {username}")
 
-                elif gövde == "exit":
-                    print("programdan çıkıldı")
-                    break
+    def komut_info(self):
+        print("--sistem bilgileri--")
+        print(f"OS Adı: {os.name}")
+        try:
+            print(os.uname())
+        except AttributeError:
+            print("uname() bu sistemde desteklenmiyor.")
+        print(f"PATH: {os.getenv('PATH')}")
+        print("kullanılan terminal türü: Ceaser Terminal")
 
-                else:
-                    print("Geçersiz komut. 'help' yazarak komutları görebilirsiniz.")
-            except Exception as e:
-                print(f"Hata oluştu: {e}")
+    def komut_timelook(self):
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        print(f"Güncel zaman: {current_time}")
+
+    def komut_log(self):
+        sifre = input("şifreniz: ")
+        if sifre == PASSWORD:
+            print("log erişimi verildi")
+            if os.path.exists(LOG_DOSYA):
+                with open(LOG_DOSYA, "r", encoding="utf-8") as f:
+                    icerik = f.read()
+                    if icerik.strip():
+                        print("-- log kayıtları --")
+                        print(icerik)
+                    else:
+                        print("log boş")
+            else:
+                print("henüz log oluşturulmadı")
+        else:
+            print("yanlış şifre")
+
+    def komut_log_clear(self):
+        if os.path.exists(LOG_DOSYA):
+            open(LOG_DOSYA, "w").close()
+            print("log temizlendi")
+        else:
+            print("log dosyası bulunamadı")
+
+    def komut_userinfo(self):
+        username = getpass.getuser()
+        print(f"Kullanıcı adı: {username}")
+
+    def komut_history(self):
+        if self.komut_gecmisi:
+            print("-- KOMUT GEÇMİŞİ --")
+            for i, cmd in enumerate(self.komut_gecmisi, 1):
+                print(f"{i}. {cmd}")
+        else:
+            print("henüz komut geçmişi yok")
+
+    def komut_yzgame(self):
+        while True:
+            secenekler = ["yazı", "tura"]
+            soru = input("Yazı mı Tura mı? (çıkmak için 'exit'): ").strip().lower()
+
+            if soru == "exit":
+                print("Yazı tura oyunu kapatıldı.")
+                break
+            elif soru not in secenekler:
+                print("Lütfen sadece 'yazı' veya 'tura' yazın.")
+                continue
+
+            sans = random.choice(secenekler)
+            if soru == sans:
+                print(f" Tebrikler! Bildiniz: Çıkan sonuç -> {sans}")
+            else:
+                print(f" Maalesef, bilemediniz. Seçiminiz: {soru}, Çıkan: {sans}")
+
+    def komut_lucky(self):
+        sayi = random.randint(1, 100)
+        print(f"{sayi} şanslı sayınız.")
 
 
 # Program başlat
